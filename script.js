5c9e0d150a0f8b62571bd2d4c512689b9916eb0c
Maydelle <maydellemc@gmail.com>
2024-12-13 13:07:41 +1000

    Creating new function to update weather data

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
 function searchCity(city) {
   let apiKey = "obc25ed040a8998de4b43f2ea367t025";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  console.log(apiUrl);
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
