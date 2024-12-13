2690de6dfcd413ec886f7e6a09e8360bba9a546f
Maydelle <maydellemc@gmail.com>
2024-12-13 12:08:31 +1000

    Added basic search engine javascript

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let weatherCityElement = document.querySelector("#weather-city");
+  weatherCityElement.innerHTML = searchInput.value;
+}
+
 let searchFormElement = document.querySelector("#search-form");
-console.log(searchFormElement);
+searchFormElement.addEventListener("submit", handleSearchSubmit);
