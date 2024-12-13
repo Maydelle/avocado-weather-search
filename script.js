a770b0c3c916f0acbe18fd261267ecb99d3e01af
Maydelle <maydellemc@gmail.com>
2024-12-13 12:48:12 +1000

    Added apiKey and apiUrl

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
+function searchCity(city) {
+  let apiKey = "obc25ed040a8998de4b43f2ea367t025";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}$units=metric`;
+  console.log(apiUrl);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let weatherCityElement = document.querySelector("#weather-city");
+
   weatherCityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
