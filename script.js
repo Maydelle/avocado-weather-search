ff2ffe9ee0ec156f31353132d4a3c6f1b4026400
Maydelle <maydellemc@gmail.com>
2024-12-13 13:02:12 +1000

    Fixed error in apiUrl

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function searchCity(city) {
   let apiKey = "obc25ed040a8998de4b43f2ea367t025";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}$units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   console.log(apiUrl);
 }
 
