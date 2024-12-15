294594a86d9812788b91e6eb91b40c3e6a49b6dd
Maydelle <maydellemc@gmail.com>
2024-12-15 13:49:08 +1000

    Added search functionality with API

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function refreshWeather(response) {
-  console.log(response.data);
+  let temperatureElement = document.querySelector("#weather-value");
+  let temperature = response.data.temperature.current;
+  let weatherCityElement = document.querySelector("#weather-city");
+
+  weatherCityElement.innerHTML = response.data.city;
+
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function searchCity(city) {
@@ -11,11 +17,11 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let weatherCityElement = document.querySelector("#weather-city");
 
-  weatherCityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Brisbane");
