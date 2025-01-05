188c48e286f9eb6548e5ebda0e9d9edd7e5f84f5
Maydelle <maydellemc@gmail.com>
2025-01-05 13:49:59 +1000

    Replaced dummy content with API content inside of HTML with javascript and styled

--- script.js
+++ script.js
@@ -52,14 +52,49 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
-
 function getForecast(city) {
   let apiKey = "obc25ed040a8998de4b43f2ea367t025";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
-function displayForecast(response) {}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+    <div class="weather-forecast-day">
+            <div class="weather-forecast-date">${formatDay(day.time)}</div>
+            <div ><img src="${
+              day.condition.icon_url
+            }" class="weather-forecast-icon"></div>
+            <div class="weather-forecast-temps">
+              <span class="weather-forecast-min">${Math.round(
+                day.temperature.minimum
+              )}°C</span>
+              <span class="divider">|</span>
+              <span class="weather-forecast-max"><strong>${Math.round(
+                day.temperature.maximum
+              )}°C</strong></span>
+            </div>
+          </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
